ea5b2072bd37696e34148bf41753eb0246771f41
seoHyuongmin <hyuongmin.seo@munto.kr>
2022-06-30 12:48:11 +0900

    [UPDATE] delete

--- src/components/SingleToDo.tsx
+++ src/components/SingleToDo.tsx
@@ -1,43 +1,49 @@
-import React from 'react'
-import { ToDo } from '../model';
+import React from "react";
+import { ToDo } from "../model";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
 import "./style.css";
 
 type Props = {
-    toDo: ToDo;
-    toDos: ToDo[];
-    setToDos: React.Dispatch<React.SetStateAction<ToDo[]>>;
+  toDo: ToDo;
+  toDos: ToDo[];
+  setToDos: React.Dispatch<React.SetStateAction<ToDo[]>>;
 };
 
 const SingleToDo = ({ toDo, toDos, setToDos }: Props) => {
-    const handleDone = (id: number) => {
-        setToDos(
-            toDos.map((todo) =>
-                todo.id === id ? { ...toDo, isDone: !todo.isDone } : todo)
-        );
-    };
-    return (
-        <form className="toDos__single">
-            {toDo.isDone ? (
-                <span className="toDos__single--text">{toDo.toDo} true</span>
-            ) : (
-                <span className="toDos__single--text">{toDo.toDo}</span>
-            )}
-            {/* <span className="toDos__single--text">{toDo.toDo}</span> */}
-            <div>
-                <span className="icon">
-                    <AiFillEdit />
-                </span>
-                <span className="icon">
-                    <AiFillDelete />
-                </span>
-                <span className="icon" onClick={() => handleDone(toDo.id)}>
-                    <MdDone />
-                </span>
-            </div>
-        </form>
-    )
-}
+  const handleDone = (id: number) => {
+    setToDos(
+      toDos.map((todo) =>
+        todo.id === id ? { ...toDo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
 
-export default SingleToDo
+  const handleDelete = (id: number) => {
+    const resultArr = toDos.filter((todo) => todo.id !== id);
+    setToDos(resultArr);
+  };
+
+  return (
+    <form className="toDos__single">
+      {toDo.isDone ? (
+        <span className="toDos__single--text">{toDo.toDo} true</span>
+      ) : (
+        <span className="toDos__single--text">{toDo.toDo}</span>
+      )}
+      <div>
+        <span className="icon">
+          <AiFillEdit />
+        </span>
+        <span className="icon" onClick={() => handleDelete(toDo.id)}>
+          <AiFillDelete />
+        </span>
+        <span className="icon" onClick={() => handleDone(toDo.id)}>
+          <MdDone />
+        </span>
+      </div>
+    </form>
+  );
+};
+
+export default SingleToDo;
